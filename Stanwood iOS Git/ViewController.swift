3f6227230cba468ff3d09e244ef8435330d52bbc
Nika Kirkitadze <nikakirkitadze@icloud.comm>
2020-07-30 23:11:41 +0400

    Initial commit

---
+++ Stanwood iOS Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stanwood iOS Git
+//
+//  Created by Nika Kirkitadze on 7/30/20.
+//  Copyright © 2020 Stanwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
